bf5764eae155463fb2b50e939430de47ef5b2270
Nihilth <91199065+NIhilth@users.noreply.github.com>
2021-12-09 19:28:05 -0300

    criei todas as coisinha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CalculadoraComponent } from './main/calculadora/calculadora.component';
+import { VisorComponent } from './main/calculadora/visor/visor.component';
+import { BotaoComponent } from './main/calculadora/botao/botao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CalculadoraComponent,
+    VisorComponent,
+    BotaoComponent
   ],
   imports: [
     BrowserModule
