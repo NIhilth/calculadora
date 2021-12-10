aba60405693df818de10bf85beaf60ff9d420a90
Nihilth <91199065+NIhilth@users.noreply.github.com>
2021-12-10 20:28:51 -0300

    aaaaaaaa

--- src/app/main/calculadora/calculadora.component.ts
+++ src/app/main/calculadora/calculadora.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
@@ -10,19 +10,28 @@ export class CalculadoraComponent implements OnInit {
   constructor() { }
 
   conta = ""
-  
+  checagem = ""
+
   ngOnInit() {
   }
 
-  passarNumeroVisor(digito){
-    this.conta += digito
+  passarNumeroVisor(digito) {
+    if(digito != "+" && digito != "*" && digito != "/" && digito != "-"){
+      if(this.conta == "+" || this.conta == "*" || this.conta == "/" || this.conta == "-"){
+        this.conta = this.conta.substring(1)
+      }
+      this.conta += digito
+    } else if (this.checagem != "+" && this.checagem != "*" && this.checagem != "/" && this.checagem != "-") {
+      this.conta += digito
+    }
+    this.checagem = digito
   }
 
-  resultado(){
+  resultado() {
     this.conta = eval(this.conta)
   }
 
-  restartar(){
+  restartar() {
     this.conta = ""
   }
 
