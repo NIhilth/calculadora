1ac9ffd545346fcfb7707e77ab795c876c650ba7
Nihilth <91199065+NIhilth@users.noreply.github.com>
2021-12-10 21:35:02 -0300

    mais coisa

--- src/app/main/calculadora/calculadora.component.ts
+++ src/app/main/calculadora/calculadora.component.ts
@@ -11,28 +11,49 @@ export class CalculadoraComponent implements OnInit {
 
   conta = ""
   checagem = ""
+  tamanho = 0
+  condicao = 0
 
   ngOnInit() {
   }
 
   passarNumeroVisor(digito) {
-    if(digito != "+" && digito != "*" && digito != "/" && digito != "-"){
-      if(this.conta == "+" || this.conta == "*" || this.conta == "/" || this.conta == "-"){
+    if (this.condicao == 1 && (this.checagem == "+" || this.checagem == "*" || this.checagem == "/" || this.checagem == "-" || this.checagem == "(")) {
+      this.conta += digito
+      this.condicao = 0
+    } else if (digito != "+" && digito != "*" && digito != "/" && digito != "-") {
+      if (this.conta == "+" || this.conta == "*" || this.conta == "/" || this.conta == "-") {
         this.conta = this.conta.substring(1)
       }
       this.conta += digito
-    } else if (this.checagem != "+" && this.checagem != "*" && this.checagem != "/" && this.checagem != "-") {
+    } else if (this.checagem != "+" && this.checagem != "*" && this.checagem != "/" && this.checagem != "-" && this.checagem != "(") {
       this.conta += digito
-    }
+    } 
+    
     this.checagem = digito
   }
 
   resultado() {
-    this.conta = eval(this.conta)
+    this.tamanho = this.conta.length
+    this.checagem = this.conta.substring(this.tamanho - 1)
+    if (this.checagem != "+" && this.checagem != "*" && this.checagem != "/" && this.checagem != "-" && this.checagem != "(") {
+      this.conta = eval(this.conta)
+      this.conta = this.conta
+    } else {
+      this.conta = this.conta.substring(0, this.tamanho - 1)
+      this.condicao = 1
+    }
+
   }
 
   restartar() {
     this.conta = ""
+  }
+
+  apagarValor(){
+    this.tamanho = this.conta.length
+    this.conta = this.conta.substring(0, this.tamanho - 1)
+    this.conta = this.conta
   }
 
 }
